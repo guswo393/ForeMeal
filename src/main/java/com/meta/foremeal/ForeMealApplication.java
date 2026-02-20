670f444371925c4b967b0f46c99874acd443150e
guswo393 <seohyun3911@g.hongik.ac.kr>
2026-02-20 22:13:02 +0900

    chore: 초기 프로젝트 구조 생성

---
+++ src/main/java/com/meta/foremeal/ForeMealApplication.java
@@ -0,0 +1,13 @@
+package com.meta.foremeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForeMealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForeMealApplication.class, args);
+	}
+
+}
