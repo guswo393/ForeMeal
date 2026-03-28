44c6405fb11f7ea7d8c5a6839b39bce8de8c80e0
psxo3 <pkse11283@gmail.com>
2026-03-28 17:37:18 +0900

    commit: 로그인&회원가입 페이지 생성 및 수정

--- src/App.jsx
+++ src/App.jsx
@@ -6,45 +6,78 @@ import GlucosePage from './pages/GlucosePage'
 import MealRecommendPage from './pages/MealRecommendPage'
 import MyPage from './pages/MyPage'
 import LoginPage from './pages/LoginPage'
+import SignupPage from './pages/SignupPage'
 
 function App() {
-
-  const [isLoggedIn, setIsLoggedIn] = useState(true)
-  const [currentPage, setCurrentPage] = useState('mypage')
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [authPage, setAuthPage] = useState('login')
+  const [currentPage, setCurrentPage] = useState('home')
 
   const [userProfile, setUserProfile] = useState({
-    userId: "plurie01",
-    nickname: "플러이",
-    weight: "26",
-    height: "160",
-    birth: "2025-04-19"
+    userId: '',
+    nickname: '',
+    weight: '',
+    height: '',
+    birth: ''
+  })
+
+  const [signupUser, setSignupUser] = useState({
+    userId: '',
+    password: '',
+    nickname: '',
+    weight: '',
+    height: '',
+    birth: ''
   })
 
   if (!isLoggedIn) {
-    return <LoginPage setIsLoggedIn={setIsLoggedIn} />
+    if (authPage === 'signup') {
+      return (
+        <SignupPage
+          setAuthPage={setAuthPage}
+          signupUser={signupUser}
+          setSignupUser={setSignupUser}
+        />
+      )
+    }
+
+    return (
+      <LoginPage
+        setIsLoggedIn={setIsLoggedIn}
+        setUserProfile={setUserProfile}
+        setAuthPage={setAuthPage}
+        signupUser={signupUser}
+      />
+    )
   }
 
   const renderPage = () => {
-
-    if (currentPage === 'home')
+    if (currentPage === 'home') {
       return <HomePage userName={userProfile.nickname} />
+    }
 
-    if (currentPage === 'glucose')
+    if (currentPage === 'glucose') {
       return <GlucosePage />
+    }
 
-    if (currentPage === 'meal')
+    if (currentPage === 'meal') {
       return <MealRecommendPage />
+    }
 
-    if (currentPage === 'mypage')
-      return <MyPage userProfile={userProfile} setUserProfile={setUserProfile} />
+    if (currentPage === 'mypage') {
+      return (
+        <MyPage
+          userProfile={userProfile}
+          setUserProfile={setUserProfile}
+        />
+      )
+    }
 
     return <HomePage userName={userProfile.nickname} />
-
   }
 
   return (
     <div className="app-container">
-
       <Header currentPage={currentPage} />
 
       <main className="main-content">
@@ -55,10 +88,8 @@ function App() {
         currentPage={currentPage}
         setCurrentPage={setCurrentPage}
       />
-
     </div>
   )
-
 }
 
 export default App
---
+++ src/pages/SignupPage.jsx
@@ -0,0 +1,138 @@
+import { useState } from 'react'
+import '../styles/SignupPage.css'
+
+function SignupPage({ setAuthPage, signupUser, setSignupUser }) {
+  const [form, setForm] = useState({
+    userId: signupUser.userId || '',
+    password: signupUser.password || '',
+    passwordCheck: '',
+    nickname: signupUser.nickname || '',
+    weight: signupUser.weight || '',
+    height: signupUser.height || '',
+    birth: signupUser.birth || ''
+  })
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+
+    setForm((prev) => ({
+      ...prev,
+      [name]: value
+    }))
+  }
+
+  const handleSignup = () => {
+    const { userId, password, passwordCheck, nickname, weight, height, birth } = form
+
+    if (!userId || !password || !passwordCheck || !nickname || !weight || !height || !birth) {
+      alert('모든 항목을 입력해주세요.')
+      return
+    }
+
+    if (password !== passwordCheck) {
+      alert('비밀번호가 일치하지 않습니다.')
+      return
+    }
+
+    setSignupUser({
+      userId,
+      password,
+      nickname,
+      weight,
+      height,
+      birth
+    })
+
+    alert('회원가입이 완료되었습니다. 로그인 해주세요.')
+    setAuthPage('login')
+  }
+
+  return (
+    <div className="signup-container">
+      <div className="signup-box">
+        <h1 className="signup-title">회원가입</h1>
+        <p className="signup-subtitle">ForeMeal 계정을 만들어보세요</p>
+
+        <input
+          type="text"
+          name="userId"
+          placeholder="아이디"
+          value={form.userId}
+          onChange={handleChange}
+          className="signup-input"
+        />
+
+        <input
+          type="password"
+          name="password"
+          placeholder="비밀번호"
+          value={form.password}
+          onChange={handleChange}
+          className="signup-input"
+        />
+
+        <input
+          type="password"
+          name="passwordCheck"
+          placeholder="비밀번호 확인"
+          value={form.passwordCheck}
+          onChange={handleChange}
+          className="signup-input"
+        />
+
+        <input
+          type="text"
+          name="nickname"
+          placeholder="닉네임"
+          value={form.nickname}
+          onChange={handleChange}
+          className="signup-input"
+        />
+
+        <input
+          type="text"
+          name="weight"
+          placeholder="체중"
+          value={form.weight}
+          onChange={handleChange}
+          className="signup-input"
+        />
+
+        <input
+          type="text"
+          name="height"
+          placeholder="키"
+          value={form.height}
+          onChange={handleChange}
+          className="signup-input"
+        />
+
+        <input
+          type="date"
+          name="birth"
+          value={form.birth}
+          onChange={handleChange}
+          className="signup-input"
+        />
+
+        <button
+          type="button"
+          className="signup-submit-button"
+          onClick={handleSignup}
+        >
+          회원가입 완료
+        </button>
+
+        <button
+          type="button"
+          className="signup-back-button"
+          onClick={() => setAuthPage('login')}
+        >
+          로그인으로 돌아가기
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default SignupPage
