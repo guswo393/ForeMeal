b191e837314f952d6be99f0dfc9cf078d08e72a6
seoyeonpark906 <lilycole1124@gmail.com>
2026-03-28 17:27:54 +0900

    Refactor: 멘트 수정

--- src/main/java/com/meta/foremeal/health/exception/HealthProfileNotFoundException.java
+++ src/main/java/com/meta/foremeal/health/exception/HealthProfileNotFoundException.java
@@ -2,6 +2,6 @@ package com.meta.foremeal.health.exception;
 
 public class HealthProfileNotFoundException extends RuntimeException {
     public HealthProfileNotFoundException() {
-        super("건강 프로필을 찾을 수 없습니다.");
+        super("프로필을 찾을 수 없습니다.");
     }
 }
