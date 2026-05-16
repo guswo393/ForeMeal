9030e61a15da4762e097e72627c3c3568be9e460
guswo393 <seohyun3911@g.hongik.ac.kr>
2026-05-16 15:22:56 +0900

    chore: vital data value 엔티티 생성

--- src/main/java/com/meta/foremeal/VitalData/VitalDataValue_domain.java
+++ src/main/java/com/meta/foremeal/VitalData/VitalDataValue_domain.java
@@ -6,7 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
+import jakarta.persistence.Id;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
